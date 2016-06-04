61d1a8cb5b62006cc67d73773d1cd228aa657c7f
teheavy <fredchen666@gmail.com>
2016-06-04 16:33:06 -0400

    New user for macbook

---
+++ saoye/updates/0.0.2-admins-mac.js
@@ -0,0 +1,9 @@
+/* 
+ * Create a new user for macbook.
+ */
+
+exports.create = {
+	User: [
+		{ 'name.first': 'Mac-Admin', 'name.last': 'User', email: 'admin@keystonejs.com', password: 'admin', isAdmin: true }
+	]
+};
