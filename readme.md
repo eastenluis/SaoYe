912f8fc9a9f29b136260691c6cf4d37e11040051
martin.lai <martin.lai@caseware.com>
2016-09-28 15:00:55 -0400

    Add simple license

--- readme.md
+++ readme.md
@@ -1,26 +1,3 @@
-# SaoYe
+# SaoYe | 骚也
 
-This is the source code of SaoYe (骚也) website.
-
-## Set up.
-
-You need MongoDB and NodeJS to run SaoYe in the local.
-
-1. In sub-directory `saoye`, run `npm install`.
-2. Within the directory, create file `.env`. Type in the following content in `.env` file:
-
-```javascript
-NODE_ENV="development"
-COOKIE_SECRET=<COOKIE_SECRET> // Just some random string is fine.
-``` 
-
-3. Ensure your mongo service is run. Then run `npm start`. The website should be able to
- access from `localhost:3000`.
-
-## Deployment
-
-You have to have the proper permission on heroku dyno.
-
-1. In you repo, `git remote add heroku <path-to-git-repository>`.
-2. `git fetch heroku`.
-3. When you need to deploy, (in root repository) use `git push heroku:master --force`.
+This is the source repository of website SaoYe (骚也), built upon [KeystnoeJS](http://keystonejs.com/).
--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -6,44 +6,8 @@
  * Alternatively, you can export a custom function for the update:
  * module.exports = function(done) { ... }
  */
-
 exports.create = {
 	User: [
 		{ 'name.first': 'Admin', 'name.last': 'User', email: 'user@keystonejs.com', password: 'admin', isAdmin: true }
 	]
 };
-
-/*
-
-// This is the long-hand version of the functionality above:
-
-var keystone = require('keystone'),
-	async = require('async'),
-	User = keystone.list('User');
-
-var admins = [
-	{ email: 'user@keystonejs.com', password: 'admin', name: { first: 'Admin', last: 'User' } }
-];
-
-function createAdmin(admin, done) {
-	
-	var newAdmin = new User.model(admin);
-	
-	newAdmin.isAdmin = true;
-	newAdmin.save(function(err) {
-		if (err) {
-			console.error("Error adding admin " + admin.email + " to the database:");
-			console.error(err);
-		} else {
-			console.log("Added admin " + admin.email + " to the database.");
-		}
-		done(err);
-	});
-	
-}
-
-exports = module.exports = function(done) {
-	async.forEach(admins, createAdmin, done);
-};
-
-*/
