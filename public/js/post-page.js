edae3cadc836603a825e04e6ec9b4a360f3d0acd
martin.lai <martin.lai@caseware.com>
2016-10-04 15:23:06 -0400

    Fix tooltip trigger.

--- public/js/post-page.js
+++ public/js/post-page.js
@@ -1,11 +1,14 @@
-(function () {
+(function() {
     'use strict';
 
     // Prepare all tooltips.
-    $('[data-toggle="tooltip"]').tooltip({ trigger: 'click' });
+    $('[data-toggle="tooltip"]').tooltip({ trigger: 'manual' })
+        .click(function() {
+            $(this).tooltip('show');
+        });
+    // Hide tooltip when body clicked.
     $('body').on('click', function(evt) {
-        if (evt.target && evt.target.getAttribute('data-toggle') === 'tooltip')
-            return false;
-        $('[data-toggle="tooltip"]').tooltip('hide');
+        if (!evt.target || evt.target.getAttribute('data-toggle') !== 'tooltip')
+            $('[data-toggle="tooltip"]').tooltip('hide');
     });
 })();
